3c1cb96213c112adc7b8f073c1c5c2e345a97473
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-08-20 17:29:20 -0400

    create routes GET and POST

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index); // -> GET ONG
 routes.post('/ongs', OngController.create); // -> POST ONG
+
+routes.get('/incidents', IncidentController.index); // -> GET INCIDENT
+routes.post('/incidents', IncidentController.create); // -> POST INCIDENT
 
 module.exports = routes;
