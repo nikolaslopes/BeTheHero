cad07ac4b34ab746e6bd5e1a302d79a8c08836d7
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-08-20 16:04:37 -0400

    adjusted routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,24 +1,10 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+
+const OngController = require('./controllers/OngController');
 
 const routes = express.Router();
 
-routes.post('/ongs', async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body;
-
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    email,
-    name,
-    whatsapp,
-    city,
-    uf,
-  });
-
-  return response.json({ id });
-});
+routes.get('/ongs', OngController.index); // -> GET ONG
+routes.post('/ongs', OngController.create); // -> POST ONG
 
 module.exports = routes;
