7812a479813f4c955881c14afc418edd62eca762
Nix <nikolaslopes.dev@gmail.com>
2021-08-22 14:50:47 -0400

    import cors

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const routes = require('./src/routes');
 
 const app = express();
 
-// -> Antes de todas as requests, pegar o body da request(JSON) e converter em algo entendivel pela aplicação
-app.use(express.json());
-
-/**
- * Rota / Recurso -> (http:/users)
- */
-
-/**
- * Métodos HTTP:
- * GET: Buscar/listar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- */
-
-/**
- * Tipos de parâmetros:
- *
- * Query Params: Parâmetros nomeados enviados na rota após "?" (filtros, paginação)
- * Route Params: Parâmetros utilizados para identificar recursos (hppt:/users/:id)
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
- */
-
-app.get('/users', (request, response) => {
-
-  return response.json({
-    yourName: 'nick',
-    userName: 'nicklps',
-  });
-});
+app.use(cors());
+app.use(express.json()); // -> Response JSON types
+app.use(routes);
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
