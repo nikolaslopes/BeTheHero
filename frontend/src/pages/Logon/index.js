28d2fdb33a2945a13c9b310aee2eac502715ea25
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-08-23 14:23:42 -0400

    created Logon page

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,30 @@
+import './styles.css';
+
+import { FiLogIn } from 'react-icons/fi';
+
+import logoImg from '../../assets/images/logo.svg';
+import heroesImg from '../../assets/images/heroes.png';
+
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Logo Be The Hero" />
+
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua ID" />
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
+}
