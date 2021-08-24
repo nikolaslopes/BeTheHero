27a83d0750fe123e4149436369dea9de2fcea406
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-08-24 12:58:38 -0400

    added useEffect to load cases

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -10,6 +10,8 @@ import logoImg from '../../assets/images/logo.svg';
 
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
+
+  const history = useHistory();
 
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
@@ -38,6 +40,12 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -51,7 +59,11 @@ export default function Profile() {
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
         </Link>
-        <button type="button">
+
+        <button
+          type="button"
+          onClick={handleLogout}
+        >
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
