280e3c622ba543a22ed4be599b255ddf878c05db
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-08-24 13:01:51 -0400

    changed local routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
+app.use(express.json()); // -> Response JSON types
 app.use(routes);
 
 app.listen(3333, () => console.log('🔥 Server started at http://localhost:3333'));
