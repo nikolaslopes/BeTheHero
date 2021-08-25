54ad4d5e4033ac177efe3148b1cc98a6d1368fb7
nikolaslopes <nikolaslopes.dev@gmail.com>
2021-08-25 17:09:20 -0400

    added root-loader

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,4 +1,5 @@
-import { Switch, Route } from 'react-router-dom';
+import { useLocation, Switch, Route } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
 
 import Home from './pages/Home';
 import Register from './pages/Register';
@@ -6,12 +7,33 @@ import Profile from './pages/Profile';
 import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/register" component={Register} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/incidents/new" component={NewIncident} />
-    </Switch>
-  );
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: {
+      opacity: 0,
+      position: 'absolute',
+      transform: 'translateY(40px)',
+    },
+    enter: {
+      opacity: 1,
+      posisiton: 'absolute',
+      transform: 'translateY(0px)',
+    },
+    leave: {
+      opacity: 0,
+      position: 'absolute',
+      transform: 'transolteX(40px)',
+    },
+  });
+
+  return transitions((props, item) => (
+    <animated.div style={props}>
+      <Switch location={item}>
+        <Route path="/" exact component={Home} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
+      </Switch>
+    </animated.div>
+  ));
 }
