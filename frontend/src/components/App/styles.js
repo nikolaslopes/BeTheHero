c7207cea44ff592c02880b12dd482d0df8ff1c6d
Nix <nikolaslopes.dev@gmail.com>
2021-08-25 07:15:37 -0400

    created container on App with SC

--- frontend/src/components/App/styles.js
+++ frontend/src/components/App/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 500px;
-  margin: auto auto;
+  max-width: 1120px;
+  margin: auto;
   padding: 0 16px;
 `;
